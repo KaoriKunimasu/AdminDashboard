fb4bfd43af9cc225036e8448f6709c507eaec76e
Kaori <ichika200010@gmail.com>
2025-04-08 06:16:10 +1200

    Updated next.config

--- next.config.ts
+++ next.config.ts
@@ -9,11 +9,8 @@ const nextConfig: NextConfig = {
     GOOGLE_APPLICATION_CREDENTIALS_JSON: process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON,
     GOOGLE_ANALYTICS_PROPERTY_ID: process.env.GOOGLE_ANALYTICS_PROPERTY_ID,
   },
-  // Explicitly configure to use webpack
-
-  // Ensure we're using the webpack configuration
   webpack: (config, { isServer }) => {
-    // Any custom webpack configurations if needed
+    // Add custom webpack configuration here if needed
     return config;
   },
 };
